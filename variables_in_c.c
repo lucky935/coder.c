1d8c6f9b3c61d5cf915391f9c8c4d5c2c9ab27f8
lucky935 <84118606+lucky935@users.noreply.github.com>
2021-05-14 23:29:34 +0530

    Create variables_in_c.c

---
+++ variables_in_c.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(){
+
+	int hour;
+	int min;
+	int sec;
+	hour = 4;
+	min =23;
+	sec =4;
+	printf("hour = %d \n",4);
+	printf("min = %d\n",23);
+	printf("sec = %d\n",4);
+
+
+
+}
