e885b01900e77566b8e145ea695d60b68d53dfb6
lucky935 <84118606+lucky935@users.noreply.github.com>
2021-05-14 23:00:02 +0530

    Create writing cooments in c.c

---
+++ writing cooments in c.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main(){
+	//hey!wass'up dude
+	/*In c programming
+	pointers
+	are very difficult*/
+//this is single line comment
+/*this is 
+multiline 
+comment*/
+	printf("comments will not print in output");
+	return 0;	
+
+}
