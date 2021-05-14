4431fc42532e8b950cb3aec29ff8f1170bcb0d73
lucky935 <84118606+lucky935@users.noreply.github.com>
2021-05-15 01:24:07 +0530

    Create const.c

---
+++ const.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main(void){
+	// float constant
+	float const pi = 3.14;
+
+	//integer
+	int const a =3;
+
+	//charcter
+	char const yes = "y";
+
+	printf("float = %f\n",pi);
+	printf("int = %d\n",a);
+	printf("char = %c\n",yes);
+	return 0;
+	
+
+}
