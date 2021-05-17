732c9eef18ea1c37929cdce4800ab2acc227ef2d
lucky935 <84118606+lucky935@users.noreply.github.com>
2021-05-17 23:59:06 +0530

    Create program_to _square a_number.c

---
+++ program_to _square a_number.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main(void) {
+  int var1;
+	printf("enter an integer to get square value: ");
+	scanf("%d",&var1);
+	printf("the square of given number is:%d",var1*var1);
+
+}
