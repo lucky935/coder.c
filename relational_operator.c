f2fb078dc0904888c142439fe30f5be819f67f9d
lucky935 <84118606+lucky935@users.noreply.github.com>
2021-05-20 11:34:01 +0530

    Create relational_operator.c

---
+++ relational_operator.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(void) {
+  
+	int a,b;
+	scanf("%d %d",&a,&b);
+	printf("check equality between a and b:%d\n",a==b);
+	printf("check equality between a and b:%d\n",a!=b);
+	printf("check equality between a and b:%d\n",a>b);
+	printf("check equality between a and b:%d\n",a<b);printf("check equality between a and b:%d\n",a>=b);
+	printf("check equality between a and b:%d\n",a<=b);
+
+
+}
